61a7dcef3d499cdde72ad8d72dee93f79ae0043e
yassun7010 <yassun7010@outlook.com>
2024-01-04 20:04:23 +0900

    chore: update command interface.

--- src/main.rs
+++ src/main.rs
@@ -7,32 +7,21 @@ use clap::Parser;
 enum App {
     /// Inject environment variables into shell command
     Inject(InjectArgs),
-    Sync(SyncArgs),
 }
 
 #[derive(clap::Args)]
 struct InjectArgs {
-    #[arg(long)]
-    no_cache: bool,
-    #[arg(long)]
-    scope: Option<String>,
+    #[arg(short, long, default_value = ".env")]
+    envfile: String,
 }
-
-#[derive(clap::Args)]
-struct SyncArgs {}
 
 fn main() {
     match App::parse() {
         App::Inject(args) => inject(args),
-        App::Sync(args) => sync(args),
     }
 }
 
 fn inject(args: InjectArgs) {
     println!("Injecting...");
-    println!("No cache: {:?}", args.no_cache);
-}
-
-fn sync(_args: SyncArgs) {
-    println!("Syncing...");
+    println!("Envfile: {:?}", args.envfile);
 }
